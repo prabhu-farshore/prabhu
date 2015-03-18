60e15f53f9d800528a8c07ea2ae4606f51c5e3be
Sivaraj <ssivaraj@farshore.com>
2015-03-18 23:26:27 +0530

    Router updated with JSON parse extension to response from API in JSON format

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -35,7 +35,10 @@
  * how to customize the loading of plugin routes.
  */
 	CakePlugin::routes();
-
+        
+/*** Parsing json extension ****/
+        Router::parseExtensions('json');
+        
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
