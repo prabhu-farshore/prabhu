445ba443ba058c4afe111bde8adfa96a69f5eb77
Sivaraj <ssivaraj@farshore.com>
2015-03-19 00:08:05 +0530

    print the result from brain tree response

--- app/Controller/QlistUsersController.php
+++ app/Controller/QlistUsersController.php
@@ -64,7 +64,7 @@ class QlistUsersController extends AppController {
         
         $result['success'] = 0;
         $result['message'] = "Please check your card details.";
-        pr($result);exit;
+        pr($responseFromBrainTree);exit;
         if($responseFromBrainTree['success'] == 1){  
             $result['message'] = "Thanks..! Payment transaction completed successfully.";        
         }
