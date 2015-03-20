4699d15835e029e0757ae9e2a9c2a6d36ee38789
Sivaraj <ssivaraj@farshore.com>
2015-03-20 17:14:45 +0530

    Device id of the restaurant admin device updated in login process.

--- app/Controller/QlistAdminsController.php
+++ app/Controller/QlistAdminsController.php
@@ -96,6 +96,10 @@ class QlistAdminsController extends AppController {
                                                                                            'phone'=>$data['Restaurant']['phone'],
                                                                                            'password'=>$hashedPassword)));
             if(!empty($restaurantDetails)){
+                if($restaurantDetails['device_id']!=$data['Restaurant']['device_id']){
+                    $this->Restaurant->id = $restaurantDetails['id'];
+                    $this->Restaurant->saveField('device_id',$data['Restaurant']['device_id']);
+                }
                 $result['success'] = 1;
                 $result['message'] = "Welcome back to Q application.";
                 $result['response'] = $restaurantDetails;
